368d17c02c358f62d22b4b60dc65f0353d391cdd
Miguel Fuentes <m1gu3l.fu3nt3s@gmail.com>
2026-03-02 15:39:58 -0600

    build: generar archivos del frontend a la raiz del proyecto para github actions

--- emagro/Frontend/vite.config.js
+++ emagro/Frontend/vite.config.js
@@ -5,4 +5,8 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
     base: '/emagro/',
     plugins: [vue()],
+    build: {
+        outDir: '../',
+        emptyOutDir: false
+    }
 })
