a3040d9e1283cc3a3854ee6bd3ba9fad9a28948c
Miguel Fuentes <m1gu3l.fu3nt3s@gmail.com>
2026-03-02 15:47:11 -0600

    test: habilitar mensajes de error para debug

--- emagro/Backend/api/v1/index.php
+++ emagro/Backend/api/v1/index.php
@@ -12,9 +12,8 @@ use App\Controllers\DashboardController;
 // Backend/api/v1/index.php
 
 // 1. Load Autoloader
-// Disable HTML error output to keep JSON valid
-ini_set('display_errors', 0);
-ini_set('display_startup_errors', 0);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require_once __DIR__ . '/../../autoload.php';
