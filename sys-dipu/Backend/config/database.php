3bf7871dbdae8d6ae2474ccca66a539fc4a13524
Miguel Fuentes <m1gu3l.fu3nt3s@gmail.com>
2026-04-15 20:23:28 -0600

    fix(db): ajustar el huso horario por defecto a la hora local para la sincronizacion de mysql con el ultimo acceso

--- sys-dipu/Backend/config/database.php
+++ sys-dipu/Backend/config/database.php
@@ -9,5 +9,6 @@ return [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::ATTR_EMULATE_PREPARES => false,
+        PDO::MYSQL_ATTR_INIT_COMMAND => "SET time_zone = '-06:00'"
     ]
 ];
