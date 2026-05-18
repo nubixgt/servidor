584b0f7bd7bc8169ba3322e5b05bf91c814a5474
MEstuardoFMonzon <m1gu3l.fu3nt3s@gmail.com>
2026-05-18 10:17:54 -0600

    chore: actualizar URL del backend a producción y generar build del frontend

--- sys-dipu/Frontend/src/services/api.js
+++ sys-dipu/Frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost/IEMCOOP/template/Backend/api/v1', // Adjust this to match your local setup
+    baseURL: 'https://m.nubix.gt/sys-dipu/Backend/api/v1', // Remote backend server
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json'
