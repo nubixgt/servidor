0f87f4622dcb7720bb0820802da41387113d901d
Alex <al5772520@gmail.com>
2026-05-20 15:37:00 -0600

    fix: restaurar base URL a /sys-dipu/ para que funcione en produccion

--- sys-dipu/Frontend/vite.config.js
+++ sys-dipu/Frontend/vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/sys-dipu/Frontend/dist/',
+    base: '/sys-dipu/',
     plugins: [vue()],
 })
