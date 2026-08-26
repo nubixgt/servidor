26602d0093f0dd0c6f7bc6edf58e26cf1799d1f3
MEstuardoFMonzon <m1gu3l.fu3nt3s@gmail.com>
2026-08-26 12:15:23 -0600

    chore: Eliminar proyecto app-uba (migrado a repo independiente)

--- app-uba/app_uba/android/app/src/main/kotlin/com/example/app_uba/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.app_uba
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- app-uba/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-// web/logout.php
-session_start();
-session_destroy();
-
-// Redirigir al login con parámetro de logout exitoso
-header("Location: login.php?logout=success");
-exit;
-?>
