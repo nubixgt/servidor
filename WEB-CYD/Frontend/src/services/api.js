42e76bbe2c66dbb9400b2f126c6b6a706aef3c56
Carlos Valladares <115051589+vgcarlol@users.noreply.github.com>
2026-08-31 22:03:52 -0600

    ,

--- WEB-CYD/Frontend/src/services/api.js
+++ WEB-CYD/Frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost/IEMCOOP/template/Backend/api/v1', // Adjust this to match your local setup
+    baseURL: 'https://cyd.com.gt/Backend/',
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json'
--- WEB-CYD/Frontend/vite.config.js
+++ WEB-CYD/Frontend/vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/WEB-CYD/',
+  base: '/',
   plugins: [vue()],
   resolve: {
     alias: {
